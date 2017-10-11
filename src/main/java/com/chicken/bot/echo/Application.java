22b39682c496e9d2991a4305d1c36bace12742f1
kjs9298 <kjs9298@naver.com>
2017-10-11 18:30:24 +0900

    add image and video input type.

--- src/main/java/com/chicken/bot/echo/Application.java
+++ src/main/java/com/chicken/bot/echo/Application.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.ImageMessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.event.message.VideoMessageContent;
+import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
@@ -21,10 +24,24 @@ public class Application {
     public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
         System.out.println("event: " + event);
         return new TextMessage(event.getMessage().getText());
+    
+    }
+    
+    @EventMapping
+    public Message handleImageMessageEvent(MessageEvent<ImageMessageContent> event) {
+    	return new TextMessage("이미지로구나.");
+    
+    }
+    
+    @EventMapping
+    public Message handleVideoMessageEvent(MessageEvent<VideoMessageContent> event) {
+    	return new TextMessage("동영상이로구나.");
+    	
     }
 
     @EventMapping
     public void handleDefaultMessageEvent(Event event) {
         System.out.println("event: " + event);
+    
     }
 }
